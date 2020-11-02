a440d107514b66390219f570fab005447c3a2fa0
Gunangad Pal Singh Narula <gapsnarula@gmail.com>
2020-11-02 21:19:32 +0530

    Initialising

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = 'ows ')
+
+@client.command()
+async def load(ctx,extension):
+    client.load_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def unload(ctx,extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if(filename.endswith('.py')):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+client.run('NzMxNjY2MDE4NjA2NTE0Mjc2.XwpXFA.RHMKSQ4I4d7iX66DC4lJmJnpKjA')
---
+++ cogs/example.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+
+class Example(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.story = ""
+        self.channelid = ""
+        self.running = False
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(self.client))
+
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        if message.author == self.client.user:
+            return
+        if message.author.bot:
+            return
+        
+        ctx = await self.client.get_context(message)
+        
+        if ctx.valid == False:
+            if self.channelid == message.channel:
+                self.story += message.content + " "
+        
+    @commands.command(aliases = ['p'])
+    async def play(self, ctx):
+        if self.running:
+            await ctx.send("Already Running")
+            return
+        self.running = True
+        self.story = ""
+        self.channelid = ctx.message.channel
+        await ctx.send("Ready!")
+
+    @commands.command(aliases = ['t'])
+    async def trace(self, ctx):
+        if self.running:
+            await ctx.send(self.story)
+        return
+
+    @commands.command(aliases = ['s'])
+    async def stop(self,ctx):
+        if ctx.message.channel == self.channelid:
+            await ctx.send(self.story)
+        self.story = ""
+        self.channelid = ""
+        self.running = False
+        return
+
+    @commands.command(aliases = ['c'])
+    async def clear(self, ctx):
+        self.story = ""
+        self.channelid = ""
+        self.running = False
+
+    @commands.command(aliases = ['h'])
+    async def helpplis(self, ctx):
+            await ctx.send("play/p\nstop/s\ntrace/t\nclear/c")
+
+def setup(client):
+    client.add_cog(Example(client))
